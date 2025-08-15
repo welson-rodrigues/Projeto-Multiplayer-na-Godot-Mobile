ad4bb649bb648b203b5a228f15d1fbdc3e9340c7
welson-rodrigues <welson.rosendo@escolar.ifrn.eu.br>
2025-08-15 13:03:14 -0300

    Tilemap adicionado

--- players/player.gd
+++ players/player.gd
@@ -1,40 +1,36 @@
 extends CharacterBody2D
 
 const MAX_SPEED: float = 200.0
-const GRAVITY: float = 9.8
 const ACCELERATION: float = 500.0
-const FRICTION: float = 300.0
-const MASS: float = 45.0
+const FRICTION: float = 1000.0 
+const JUMP_VELOCITY: float = -400.0 # Pulo: Velocidade para cima (Y é negativo)
 
-#var velocity: Vector2 = Vector2.ZERO
-var _input_dir: Vector2 = Vector2.ZERO
+# Pega a gravidade das configurações do projeto para ser consistente
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _physics_process(delta: float) -> void:
+	# Adiciona a gravidade
+	# Só aplica gravidade se o personagem não estiver no chão
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+	# Lógica do pulo
+	# Verifica se a ação "jump" foi pressionada E se o personagem está no chão
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Pega a direção do input horizontal (-1 para esquerda, 1 para direita)
+	var input_axis = Input.get_axis("left", "right")
 	
-	
-func _process(_delta) -> void:
-	if _input_dir != Vector2.ZERO:
-		Client.send("position", { "x": position.x, "y": position.y })
-
-
-func _physics_process(_delta) -> void:
-	_input_dir.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-	
-	if _input_dir.length() != 0:
-		# Normalize the magnitude of the vector
-		_input_dir = _input_dir.normalized()
-		# Applies more velocity to the current velocity
-		velocity += _input_dir * ACCELERATION
-		# Don't allow higher velocity then MAX_SPEED
-		velocity = velocity.limit_length(MAX_SPEED)
+	# Movimentação Horizontal
+	if input_axis != 0:
+		# Acelera na direção do input até a velocidade máxima
+		velocity.x = move_toward(velocity.x, input_axis * MAX_SPEED, ACCELERATION * delta)
+		if is_on_floor():
+			# Envia a posição para o servidor apenas quando estiver se movendo no chão
+			Client.send("position", { "x": position.x, "y": position.y })
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
-		
-	velocity.y += MASS * GRAVITY
-		
-	set_velocity(velocity)
-	set_up_direction(Vector2.UP)
+		# Desacelera (aplica atrito) quando não há input
+		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
+
 	move_and_slide()
